1edff99042d2acb4c670977ecd9300c11371bdd8
simonhuang <thelongestusernameofall@gmail.com>
2023-08-23 15:51:56 +0800

    Add: fastchat train.py debug print

--- fastchat/train/train.py
+++ fastchat/train/train.py
@@ -288,6 +288,9 @@ def train():
     # Load data
     data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
 
+    # debug print(data_module['train_dataset'][0])
+    print(f"data_module['train_dataset'][0] = {data_module['train_dataset'][0]}")
+
     # Start trainner
     trainer = Trainer(
         model=model, tokenizer=tokenizer, args=training_args, **data_module
