eaf77f50e66dedd132287c02b48de872fcd20bea
simonhuang <thelongestusernameofall@gmail.com>
2023-08-28 18:56:20 +0800

    Merge from current working status

--- 2-start-model.sh
+++ 2-start-model.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 unset http_proxy https_proxy
+
+#running
 model=../llama-2-zh/chinese-alpaca-2-13b-sft817-v4
 
 #export CUDA_VISIBLE_DEVICES=0,1
@@ -12,4 +14,5 @@ gpu_num=2
 echo "runing ${model} with name ${name}"
 #nohup python fastchat/serve/model_worker.py --model-path ${model} --model-name ${name} --limit-worker-concurrency 30 --controller-address http://127.0.0.1:21001 --num-gpus ${gpu_num} --max-gpu-memory 35GiB --host 127.0.0.1 --port 31000 --worker http://127.0.0.1:31000  >  ./logs/model_worker.log 2>&1 &
 
+echo "running with vllm worker to accelerate"
 nohup python fastchat/serve/vllm_worker.py --model-path ${model} --model-names ${name} --limit-worker-concurrency 300 --controller-address http://127.0.0.1:21001 --num-gpus ${gpu_num} --conv-template vicuna_v1.1 --host 127.0.0.1 --port 31000 --worker-address http://127.0.0.1:31000  >  ./logs/model_worker.log 2>&1 &
