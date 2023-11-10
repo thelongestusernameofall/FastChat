7828557a0cf3419bf2bba86e11e3d0767ad8d016
simon@gput2 <simon-gpu2>
2023-11-10 15:55:26 +0800

    solve conflict

--- superadd.sh
+++ superadd.sh
@@ -3,8 +3,8 @@
 # superadd model specific layers
 #
 
-model_name=../llama-2-zh/chinese-alpaca-2-1.3b
-output_dir=../test-v2
-layers_add=1
+model_name=../llama-2-zh/chinese-alpaca-2-13b-16k
+output_dir=../test-16k
+layers_add=7
 
 python superadd/superadd.py -m ${model_name} -o ${output_dir} -a ${layers_add}
