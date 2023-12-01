e7539c967add8de437009817a94f774e9b08d93a
simonhuang <thelongestusernameofall@gmail.com>
2023-12-01 16:36:01 +0800

    Update: sample-len-histogram.py

--- extra/sample-len-histogram.py
+++ extra/sample-len-histogram.py
@@ -7,7 +7,22 @@
 
 import argparse
 import json
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+def get_sample_len(sample: dict):
+    conversations = sample.get("conversations", None)
+    if conversations:
+        user_says = conversations[0].get("value", None)
+        gpt_says = conversations[1].get("value", None)
+        if user_says and gpt_says:
+            return len(user_says) + len(gpt_says)
+        else:
+            return 0
+    return 0
 
 
 def parallel_map(func, iterable, n_jobs=-1, desc="Processing", unit="task"):
@@ -28,14 +43,16 @@ def parallel_map(func, iterable, n_jobs=-1, desc="Processing", unit="task"):
     return results
 
 
-def calculate_lengths(json_file, bin_size=10, output_file=None):
+def calculate_lengths(json_file, bin_size=10, output_file=None, jobs=8):
     with open(json_file, 'r') as file:
         data = json.load(file)
 
-    lengths = parallel_map(get_sample_len, data, desc="Calculating lengths")
+    lengths = parallel_map(get_sample_len, data, desc="Calculating lengths", n_jobs=jobs)
     lengths = list(filter(lambda x: x >= 0, lengths))
 
     max_length = max(lengths)
+    min_length = min(lengths)
+
     bins = list(range(0, max_length + bin_size, bin_size))
 
     plt.hist(lengths, bins=bins, edgecolor='black')
@@ -44,56 +61,16 @@ def calculate_lengths(json_file, bin_size=10, output_file=None):
     plt.title('Length Distribution of JSON Elements')
     plt.grid(True)
 
-    if output_file:
-        plt.savefig(output_file)
-    else:
-        plt.show()
+    x_ticks = list(range(0, max_length + 1, 500))  # 设置每100个单位一个刻度
+    plt.xticks(x_ticks, rotation=45)
 
-
-def main():
-    parser = argparse.ArgumentParser(description='Calculate lengths of JSON elements and generate a histogram')
-    parser.add_argument('--input_file', type=str, help='Path to the input JSON file', required=True)
-    parser.add_argument('--bin_size', type=int, default=10, help='Bin size for the histogram')
-    parser.add_argument('--output_file', type=str, help='Path to save the output histogram image')
-    parser.add_argument('-j', '--jobs', type=int, default=8, help='Number of jobs to run in parallel')
-
-    args = parser.parse_args()
-    calculate_lengths(args.input_file, args.bin_size, args.output_file, args.jobs)
-
-
-if __name__ == "__main__":
-    main()
-
-
-def get_sample_len(sample: dict):
-    conversations = sample.get("conversations", None)
-    if conversations:
-        user_says = conversations[0].get("value", None)
-        gpt_says = conversations[1].get("value", None)
-        if user_says and gpt_says:
-            return len(user_says) + len(gpt_says)
-        else:
-            return 0
-    return 0
-
-
-def calculate_lengths(json_file, bin_size=10, output_file=None):
-    with open(json_file, 'r') as file:
-        data = json.load(file)
-
-    lengths = [get_sample_len(element) for element in data]
-
-    max_length = max(lengths)
-    bins = list(range(0, max_length + bin_size, bin_size))
-
-    plt.hist(lengths, bins=bins, edgecolor='black')
-    plt.xlabel('Length')
-    plt.ylabel('Frequency')
-    plt.title('Length Distribution of JSON Elements')
-    plt.grid(True)
+    # 标出最大值和最小值
+    plt.text(max_length, plt.ylim()[1] * 0.9, f'Max: {max_length}', ha='right')
+    plt.text(min_length, plt.ylim()[1] * 0.9, f'Min: {min_length}', ha='left')
 
     if output_file:
         plt.savefig(output_file)
+        print(f"Saved histogram to {output_file}")
     else:
         plt.show()
 
@@ -103,9 +80,10 @@ def main():
     parser.add_argument('-i', '--input_file', type=str, help='Path to the input JSON file', required=True)
     parser.add_argument('-b', '--bin_size', type=int, default=10, help='Bin size for the histogram')
     parser.add_argument('-o', '--output_file', type=str, help='Path to save the output histogram image')
+    parser.add_argument('-j', '--jobs', type=int, default=8, help='Number of jobs to run in parallel')
 
     args = parser.parse_args()
-    calculate_lengths(args.input_file, args.bin_size, args.output_file)
+    calculate_lengths(args.input_file, args.bin_size, args.output_file, args.jobs)
 
 
 if __name__ == "__main__":
