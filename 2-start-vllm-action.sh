2bbe68232a36fcfc8ee929fa9a840701fe2fb067
simonhuang <thelongestusernameofall@gmail.com>
2024-01-04 18:33:38 +0800

    Update: 2-start-fschat-action.sh and 2-start-fschat-inference.sh

--- 2-start-vllm-action.sh
+++ 2-start-vllm-action.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-model_path=../QWen/Qwen-7B-Chat-act1227-v1f
-model_name=qwen-7b-chat
-all_gpu_num=4
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
+# previous version
+#model_path=../llama-2-zh/chinese-alpaca-2-13b-act1206-v4-t2
+# current version
+model_path=../llama-2-zh/chinese-alpaca-2-13b-act1206-v5-t2
+# testing
+#model_path=../QWen/Qwen-7B-Chat-act1227-v1f
+model_name=text-davinci-003
+
+all_gpu_num=8
 host='0.0.0.0'
 port=82
 gpu_mem_use=0.2
